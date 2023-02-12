87b1a565fbb538f74922da01a9239b2cc2a11e54
ruslol228 <88830741+ruslol228@users.noreply.github.com>
2023-02-12 20:11:52 +0400

    кодировка в win1251

--- gps.lua
+++ gps.lua
@@ -992,7 +992,12 @@ function imgui.OnDrawFrame()
                 f = io.open(getWorkingDirectory() .. "\\jumper\\gui\\gui.json", "w+")
                 f:write("[" .. text_radar_size .. ", " .. text_pos_x ..  ", " .. text_pos_y .."]")
                 f:close()
-
+                --[[
+                lua_thread.create(function ()
+                    wait(1000)
+                    thisScript():reload()
+                end)
+                ]]
             end
 
 
